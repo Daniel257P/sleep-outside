de33543f57a57391df10bb5de572da76df8c53ec
Brian Timba <bryantymbr@gmail.com>
2026-05-09 18:55:54 +0200

    done with my branch

--- src/js/product.js
+++ src/js/product.js
@@ -22,3 +22,6 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  // cart already fixed in local storage, so we can test by adding a product to cart and then refreshing the page to see if the cart is still there.
+  // no errors seen in adding item to cart and refreshing the page, so it seems to be working as expected.
