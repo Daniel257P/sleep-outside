950569566a3362d19c7680f1e8fcc8de3c02bf70
preussju <jveiga@byupathway.edu>
2026-05-14 18:57:55 +0200

    julia's team week 2 in progress

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,28 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+//import { setLocalStorage, getLocalStorage } from "./utils.mjs"; week 2
+import { getParam } from "./utils.mjs"; 
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product"); //week 2
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
 // function addProductToCart(product) {
-//   setLocalStorage("so-cart", product);
-// }
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+//week 2 - move to productdetails.mjs ^^^^^^^^^
 
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
+
+
+
+
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,11 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+//return a parameter from the URL - week 2
+export function getParam(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get('product');
+  return product;
+}
